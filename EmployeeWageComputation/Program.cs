ed9f9f700aae9535698d9046c4a63c398d14f6c1
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-03 21:00:19 +0530

    [Nikhil Kumar]:EmpCheck(present or absent) is being done

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using EmployeeWageComputation;
+using System;
 namespace EmployeWageComputation
 {
     public class Program
     {
-        public void Main()
+        public static void Main()
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            WageComputation WC = new WageComputation();
+            WC.CalculateEmpWage();
+            
         }
     }
 }
---
+++ EmployeeWageComputation/WageComputation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class WageComputation
+    {
+        int empPresent=0, empNotPresent=1;
+        public void CalculateEmpWage()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck == empPresent)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            if(empCheck == empNotPresent)
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
