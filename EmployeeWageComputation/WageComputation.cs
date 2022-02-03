a39903fb9c12534d1d8afd037dec0fb8f93e7b42
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-03 21:25:26 +0530

    [Nikhil Kumar]:PartTime Emp and PartTimeWage added

--- EmployeeWageComputation/WageComputation.cs
+++ EmployeeWageComputation/WageComputation.cs
@@ -8,12 +8,12 @@ namespace EmployeeWageComputation
 {
     public class WageComputation
     {
-        const int Full_Day_Hr = 8, WagePerHr = 20;
-        int empPresent=0, empNotPresent=1,DailyEmpWage;
+        const int Full_Day_Hr = 8, WagePerHr = 20,Part_Time_Hr=4;
+        int empPresent=0, empNotPresent=1,empPartTime=2,DailyEmpWage, PartTimeEmpWage;
         public void CalculateEmpWage()
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if(empCheck == empPresent)
             {
                 Console.WriteLine("Employee is Present");
@@ -23,10 +23,17 @@ namespace EmployeeWageComputation
             {
                 Console.WriteLine("Employee is Absent");
             }
+            if (empCheck == empPartTime)
+            {
+                Console.WriteLine("Employee is Working PartTime");
+            }
+
 
             DailyEmpWage = (Full_Day_Hr * WagePerHr);
-            Console.WriteLine("DailyEmpWage="+DailyEmpWage);
+            Console.WriteLine("Daily Employee Wage="+DailyEmpWage);
 
+            PartTimeEmpWage = (Part_Time_Hr * WagePerHr);
+            Console.WriteLine("PartTime Wage=" + PartTimeEmpWage);
         }
     }
 }
