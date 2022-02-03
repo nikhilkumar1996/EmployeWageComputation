6795eacf0383383b379ccb6852816a5df28cdcb5
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-03 20:38:37 +0530

    [Nikhil Kumar]:Welocme to EmpWageComputation program

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace EmployeWageComputation
+{
+    public class Program
+    {
+        public void Main()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
+
